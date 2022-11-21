35451e3c798c6666978c8db2ecb2dbe46f4c1aa7
Matt Stankovich <matt.stankovich@cbc.ca>
2022-11-21 10:55:27 -0500

    add funconality to update state with the order details

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -23,13 +23,23 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = (key) => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({
+      order: order,
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} index={key} />
+            ))}
           </ul>
         </div>
         <Order />
