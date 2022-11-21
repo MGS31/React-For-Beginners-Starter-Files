e484956cff15a7f6eb745013f5940999cb1319b5
Matt Stankovich <matt.stankovich@cbc.ca>
2022-11-21 15:57:34 -0500

    add editFishForm as well as functionality to showcase current fish information and ability to edit in the inventroy column

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -68,7 +68,7 @@ class App extends React.Component {
         <Inventory
           loadSampleFishes={this.loadSampleFishes}
           addFish={this.addFish}
-          fish={this.fishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
