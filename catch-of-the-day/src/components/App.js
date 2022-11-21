d9277157baaf96082adf193c1aa1df695481977c
Matt Stankovich <matt.stankovich@cbc.ca>
2022-11-21 15:06:02 -0500

    add local store references for Order state as well as update the order component to account for the firebase delay of returning data

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,6 +14,10 @@ class App extends React.Component {
 
   componentDidMount() {
     const { params } = this.props.match
+    const localStorageRef = localStorage.getItem(params.storeID);
+    if(localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef)})
+    }
     this.ref = base.syncState(`${params.storeID}/fishes`, {
       context: this,
       state: 'fishes'
@@ -22,6 +26,11 @@ class App extends React.Component {
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
+  };
+
+  componentDidUpdate() {
+    const { params } = this.props.match
+    localStorage.setItem(params.storeID, JSON.stringify(this.state.order))
   };
 
   addFish = (fish) => {
